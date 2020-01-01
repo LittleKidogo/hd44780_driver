f4e5ec0b939ce6a5f427c718baab0446269764b5
Zacck Osiemo <coderv63@gmail.com>
2020-01-01 15:46:25 +0200

    Generate scaffolding for mix project

---
+++ lib/hd44780.ex
@@ -0,0 +1,18 @@
+defmodule Hd44780 do
+  @moduledoc """
+  Documentation for Hd44780.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Hd44780.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hd44780_test.exs
@@ -0,0 +1,8 @@
+defmodule Hd44780Test do
+  use ExUnit.Case
+  doctest Hd44780
+
+  test "greets the world" do
+    assert Hd44780.hello() == :world
+  end
+end
